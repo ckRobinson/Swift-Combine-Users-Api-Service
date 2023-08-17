e1b43ceed7bbb68be9cee107e237d4558ad4f7b8
Cameron Robinson <cameron@ckrobinson.net>
2023-08-17 14:05:24 -0700

    Updated network service file.

--- SwiftCombine_Users_ApiService/Networking/UserDataService.swift
+++ SwiftCombine_Users_ApiService/Networking/UserDataService.swift
@@ -8,11 +8,129 @@
 import Foundation
 import Combine
 
+enum APIError: Error {
+    case invalidUrl
+    case invalidResponse
+    case emptyData
+    case serviceUnavailable
+    case decodingError
+    
+    var description: String {
+        switch self {
+        case .invalidUrl:
+            return "invalid url"
+        case .invalidResponse:
+            return "invalid response"
+        case .emptyData:
+            return "empty data"
+        case .serviceUnavailable:
+            return "service unavailable"
+        case .decodingError:
+            return "decoding error"
+        }
+    }
+}
+
 class UserDataService {
     
     let apiUrl: String = "https://jsonplaceholder.typicode.com/posts";
     var cancellable = Set<AnyCancellable>();
-    public func fetchData() -> Future<[UserPostData], Error> {
+    
+    public func fetchPostsUsingAsyncAwait() async throws -> [UserPostData] {
+        
+        guard let url = URL(string: self.apiUrl) else {
+            throw APIError.invalidUrl
+        }
+        let (data, response) = try await URLSession.shared.data(from: url);
+        guard let resp = response as? HTTPURLResponse,
+                  resp.statusCode == 200 else {
+            throw APIError.invalidResponse
+        }
+        return try JSONDecoder().decode([UserPostData].self, from: data)
+    }
+    
+    public func addPostUsingAsyncAwait(_ postData: UserPostData) async throws -> UserPostData {
+        
+        guard let url = URL(string: self.apiUrl) else {
+            throw APIError.invalidUrl
+        }
+        
+        var request = URLRequest(url: url);
+        request.httpMethod = "POST"
+        request.httpBody = try JSONEncoder().encode(postData);
+        request.setValue("application/json; charset=UTF-8", forHTTPHeaderField: "Content-Type");
+        
+        let (data, response) = try await URLSession.shared.data(for: request);
+        
+        guard let resp = response as? HTTPURLResponse, resp.statusCode == 201 else {
+            throw APIError.serviceUnavailable
+        }
+        
+        return try JSONDecoder().decode(UserPostData.self, from: data)
+    }
+    
+    public func putPostUsingAsyncAwait(_ postData: UserPostData) async throws -> UserPostData {
+        
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts/1") else {
+            throw APIError.invalidUrl
+        }
+        
+        var request = URLRequest(url: url);
+        request.httpMethod = "PUT"
+        request.httpBody = try JSONEncoder().encode(postData);
+        request.setValue("application/json; charset=UTF-8", forHTTPHeaderField: "Content-Type");
+        
+        let (data, response) = try await URLSession.shared.data(for: request);
+        
+        guard let resp = response as? HTTPURLResponse,
+              resp.statusCode == 200 else {
+            throw APIError.serviceUnavailable
+        }
+
+        return try JSONDecoder().decode(UserPostData.self, from: data)
+    }
+    
+    public func patchPostUsingAsyncAwait<T: Encodable>(data: [String: T]) async throws -> UserPostData  {
+        
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts/1") else {
+            throw APIError.invalidUrl
+        }
+        
+        var request = URLRequest(url: url);
+        request.httpMethod = "PATCH"
+        request.httpBody = try JSONEncoder().encode(data);
+        request.setValue("application/json; charset=UTF-8", forHTTPHeaderField: "Content-Type");
+        
+        let (data, response) = try await URLSession.shared.data(for: request);
+        
+        guard let resp = response as? HTTPURLResponse,
+              resp.statusCode == 200 else {
+            throw APIError.serviceUnavailable
+        }
+
+        return try JSONDecoder().decode(UserPostData.self, from: data)
+    }
+    
+    public func deletePostUsingAsyncAwait() async throws -> Void  {
+        
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts/1") else {
+            throw APIError.invalidUrl
+        }
+        
+        var request = URLRequest(url: url);
+        request.httpMethod = "DELETE"
+        
+        let (_, response) = try await URLSession.shared.data(for: request);
+        
+        guard let resp = response as? HTTPURLResponse,
+              resp.statusCode == 200 else {
+            throw APIError.serviceUnavailable
+        }
+
+        return
+    }
+    
+    public func fetchData_Old() -> Future<[UserPostData], Error> {
         
         return Future {[weak self] promise in
             
@@ -46,6 +164,24 @@ class UserDataService {
                 }
                 .store(in: &self.cancellable)
         }
+    }
+    
+    func fetchData() -> AnyPublisher<[UserPostData], Never> {
+        
+        guard let url = URL(string: self.apiUrl) else {
+            let subject = CurrentValueSubject<[UserPostData], Never>([])
+            return subject.eraseToAnyPublisher()
+        }
+        
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .map {
+                print("Received data. Continuing processing.")
+                return $0.data
+            }
+            .decode(type: [UserPostData].self, decoder: JSONDecoder())
+            .receive(on: RunLoop.main)
+            .replaceError(with: [])
+            .eraseToAnyPublisher()
     }
 }
 
