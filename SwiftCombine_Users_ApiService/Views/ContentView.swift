a246ce3e06a2af0a82ad4868ec6323ed7d1812fc
Cameron Robinson <cameron@ckrobinson.net>
2023-08-17 14:06:27 -0700

    Refactored content view for tab view.

--- SwiftCombine_Users_ApiService/Views/ContentView.swift
+++ SwiftCombine_Users_ApiService/Views/ContentView.swift
@@ -8,53 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
-
-    @StateObject var viewModel = UserDataViewModel()
     
     var body: some View {
         
-        NavigationView {
-         
-            ScrollView {
-                userList
-            }
-            .navigationTitle("Users")
+        TabView {
+//            FetchContentView()
+//                .tabItem {
+//                    Text("Fetch")
+//                    Image(systemName: "square.and.arrow.down")
+//                }
+            
+//            PostContentView()
+//                .tabItem {
+//                    Text("Post")
+//                    Image(systemName: "square.and.arrow.up")
+//                }
+            
+            Text("Put")
+                .tabItem {
+                    Text("Put")
+                    Image(systemName: "square.and.arrow.up.on.square")
+                }
+            
+            Text("Patch")
+                .tabItem {
+                    Text("Patch")
+                    Image(systemName: "square.and.pencil")
+                }
+            
+            Text("Delete")
+                .tabItem {
+                    Text("Delete")
+                    Image(systemName: "xmark.bin")
+                }
         }
         .padding()
-        .onAppear() {
-            viewModel.fetchData()
-        }
     }
     
-    var userList: some View {
-        ForEach(Array(self.viewModel.users.keys).sorted(), id: \.self) { userID in
-            if let user = self.viewModel.users[userID] {
-                
-                NavigationLink(destination: {
-                    UserPostingsView(userData: user)
-                        .padding(.top, -55);
-                }, label: {
-                    userListing(userData: user);
-                })
-                .foregroundColor(.black)
-            }
-        }
-    }
-    
-    private func userListing(userData: User) -> some View {
-        
-        VStack(alignment: .leading) {
-            Text("User \(userData.userID)")
-                .font(.system(size: 18).bold())
-            Text("\(userData.userPosts.count) posts")
-                .padding(.leading)
-        }
-        .padding(.vertical)
-        .padding(.horizontal)
-        .frame(maxWidth: .infinity, alignment: .leading)
-        .background(.gray.opacity(0.4))
-        .cornerRadius(15)
-    }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
