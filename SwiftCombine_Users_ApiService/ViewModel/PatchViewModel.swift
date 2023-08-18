ea6f9e2070370ebb5eb52e697700e49bf6dde338
Cameron Robinson <cameron@ckrobinson.net>
2023-08-17 19:26:33 -0700

    Updated to allow for future calls.

--- SwiftCombine_Users_ApiService/ViewModel/PatchViewModel.swift
+++ SwiftCombine_Users_ApiService/ViewModel/PatchViewModel.swift
@@ -6,13 +6,44 @@
 //
 
 import Foundation
+import Combine
 
 class PatchViewModel: ObservableObject {
     @Published var originalDataViewState: ViewState = .initial
     @Published var viewState: ViewState = .initial
     @Published var originalPostData: UserPostData? = nil
     @Published var updatedPostData: UserPostData? = nil
-    private let service: NetworkServiceAsync = NetworkServiceAsync()
+    
+    // Can't think of easy way to make this a protocol since all different return types / Async throws
+    // initing all 3 and using methods here to test each style.
+    private var cancellables = Set<AnyCancellable>()
+    let serviceType: ServiceType = .CombineFuture
+    let networkServiceAsync: NetworkServiceAsync = NetworkServiceAsync();
+    let networkServiceFuture: NetworkServiceFuture = NetworkServiceFuture();
+    let networkServicePublusher: NetworkServicePublisher = NetworkServicePublisher();
+    
+    public func patchData(postTitle: String, postBody: String) {
+        if postTitle != "" && postBody != "" {
+            self.viewState = .badInput
+            return;
+        }
+        
+        switch self.serviceType {
+            case .AsyncAwait:
+                // Not sure if this is good pracice, just using it for
+                // easy swaping to other forms of network calls in testing.
+                Task {
+                    await self.patchDataAsync(postTitle: postTitle, postBody: postBody)
+                }
+                break;
+            case .CombineFuture:
+                self.patchDataFuture(postTitle: postTitle, postBody: postBody)
+                break
+            case .CombinePublisher:
+//                self.fetchPostsPublisher()
+                break
+        }
+    }
 
     @MainActor func fetchOriginalData() {
 
@@ -20,7 +51,7 @@ class PatchViewModel: ObservableObject {
         Task {
             
             do {
-                self.originalPostData = try await service.fetchPost(postID: 1)
+                self.originalPostData = try await networkServiceAsync.fetchPost(postID: 1)
                 self.originalDataViewState = .loaded
             }
             catch {
@@ -30,12 +61,8 @@ class PatchViewModel: ObservableObject {
         }
     }
     
-    @MainActor func patchData(postTitle: String, postBody: String) {
-        
-        if postTitle != "" && postBody != "" {
-            self.viewState = .badInput
-            return;
-        }
+    @MainActor private func patchDataAsync(postTitle: String, postBody: String) {
+        print("Patching with Async")
         
         var jsonData: [String: String] = [:]
         if postTitle != "" {
@@ -49,7 +76,7 @@ class PatchViewModel: ObservableObject {
         Task {
             
             do {
-                let data = try await service.patchUserPost(data: jsonData)
+                let data = try await networkServiceAsync.patchUserPost(data: jsonData)
                 self.updatedPostData = data
                 self.viewState = .loaded
             }
@@ -58,5 +85,32 @@ class PatchViewModel: ObservableObject {
                 self.viewState = .error
             }
         }
+    }
+    
+    private func patchDataFuture(postTitle: String, postBody: String) {
+        print("Patching with Future")
+
+        var jsonData: [String: String] = [:]
+        if postTitle != "" {
+            jsonData["title"] = postTitle
+        }
+        else {
+            jsonData["body"] = postBody
+        }
+        
+        self.viewState = .loading
+        self.networkServiceFuture.patchUserPost(data: jsonData)
+            .sink(receiveCompletion: {[weak self] completion in
+                switch completion {
+                    case .finished:
+                        break
+                    case .failure(_):
+                        self?.viewState = .error
+                }
+            }, receiveValue: {[weak self] value in
+                self?.updatedPostData = value
+                self?.viewState = .loaded
+            })
+            .store(in: &self.cancellables)
     }
 }
--- SwiftCombine_Users_ApiService/ViewModel/PutViewModel.swift
+++ SwiftCombine_Users_ApiService/ViewModel/PutViewModel.swift
@@ -6,13 +6,44 @@
 //
 
 import Foundation
+import Combine
 
 class PutViewModel: ObservableObject {
     @Published var originalDataViewState: ViewState = .initial
     @Published var viewState: ViewState = .initial
     @Published var originalPostData: UserPostData? = nil
     @Published var updatedPostData: UserPostData? = nil
-    private let service: NetworkServiceAsync = NetworkServiceAsync()
+
+    // Can't think of easy way to make this a protocol since all different return types / Async throws
+    // initing all 3 and using methods here to test each style.
+    private var cancellables = Set<AnyCancellable>()
+    let serviceType: ServiceType = .CombineFuture
+    let networkServiceAsync: NetworkServiceAsync = NetworkServiceAsync();
+    let networkServiceFuture: NetworkServiceFuture = NetworkServiceFuture();
+    let networkServicePublusher: NetworkServicePublisher = NetworkServicePublisher();
+    
+    public func putData(postTitle: String, postBody: String) {
+        if postTitle == "" || postBody == "" {
+            self.viewState = .badInput
+            return;
+        }
+        
+        switch self.serviceType {
+            case .AsyncAwait:
+                // Not sure if this is good pracice, just using it for
+                // easy swaping to other forms of network calls in testing.
+                Task {
+                    await self.putDataAsync(postTitle: postTitle, postBody: postBody)
+                }
+                break;
+            case .CombineFuture:
+                self.putDataFuture(postTitle: postTitle, postBody: postBody)
+                break
+            case .CombinePublisher:
+//                self.fetchPostsPublisher()
+                break
+        }
+    }
 
     @MainActor func fetchOriginalData() {
 
@@ -20,7 +51,7 @@ class PutViewModel: ObservableObject {
         Task {
             
             do {
-                self.originalPostData = try await service.fetchPost(postID: 1)
+                self.originalPostData = try await networkServiceAsync.fetchPost(postID: 1)
                 self.originalDataViewState = .loaded
             }
             catch {
@@ -30,17 +61,14 @@ class PutViewModel: ObservableObject {
         }
     }
     
-    @MainActor func putData(postTitle: String, postBody: String) {
-        if postTitle == "" || postBody == "" {
-            self.viewState = .badInput
-            return;
-        }
-        
+    @MainActor private func putDataAsync(postTitle: String, postBody: String) {
+        print("Putting with Async")
+
         self.viewState = .loading
         Task {
             
             do {
-                let data = try await service.postUserPost(UserPostData(userID: 1, postID: 1, postTitle: postTitle, postBody: postBody))
+                let data = try await networkServiceAsync.postUserPost(UserPostData(userID: 1, postID: 1, postTitle: postTitle, postBody: postBody))
                 self.updatedPostData = data
                 self.viewState = .loaded
             }
@@ -49,5 +77,27 @@ class PutViewModel: ObservableObject {
                 self.viewState = .error
             }
         }
+    }
+    
+    private func putDataFuture(postTitle: String, postBody: String) {
+        print("Putting with Future")
+        
+        self.viewState = .loading
+        self.networkServiceFuture.putUserPost(UserPostData(userID: 1,
+                                                           postID: 1,
+                                                           postTitle: postTitle,
+                                                           postBody: postBody))
+        .sink(receiveCompletion: {[weak self] completion in
+            switch completion {
+                case .finished:
+                    break
+                case .failure(_):
+                    self?.viewState = .error
+            }
+        }, receiveValue: {[weak self] value in
+            self?.updatedPostData = value
+            self?.viewState = .loaded
+        })
+        .store(in: &self.cancellables)
     }
 }
