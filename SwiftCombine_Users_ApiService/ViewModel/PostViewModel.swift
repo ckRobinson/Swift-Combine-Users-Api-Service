9ec9e4eaa45a859dc618a0683329957b4cfa5c6b
Cameron Robinson <cameron@ckrobinson.net>
2023-08-17 19:15:38 -0700

    Updated post view model to allow for future call.

--- SwiftCombine_Users_ApiService/ViewModel/PostViewModel.swift
+++ SwiftCombine_Users_ApiService/ViewModel/PostViewModel.swift
@@ -6,13 +6,40 @@
 //
 
 import Foundation
+import Combine
 
 class PostViewModel: ObservableObject {
+    private var cancellables = Set<AnyCancellable>()
+
     @Published var viewState: ViewState = .initial
     @Published var postData: UserPostData? = nil
-    private let service: NetworkServiceAsync = NetworkServiceAsync()
     
-    @MainActor func postData(postTitle: String, postBody: String) {
+    // Can't think of easy way to make this a protocol since all different return types / Async throws
+    // initing all 3 and using methods here to test each style.
+    let serviceType: ServiceType = .CombineFuture
+    let networkServiceAsync: NetworkServiceAsync = NetworkServiceAsync();
+    let networkServiceFuture: NetworkServiceFuture = NetworkServiceFuture();
+    let networkServicePublusher: NetworkServicePublisher = NetworkServicePublisher();
+    
+    public func postData(postTitle: String, postBody: String) {
+        switch self.serviceType {
+            case .AsyncAwait:
+                // Not sure if this is good pracice, just using it for
+                // easy swaping to other forms of network calls in testing.
+                Task {
+                    await self.postDataAsync(postTitle: postTitle, postBody: postBody)
+                }
+                break;
+            case .CombineFuture:
+                self.postDataFuture(postTitle: postTitle, postBody: postBody)
+                break
+            case .CombinePublisher:
+//                self.fetchPostsPublisher()
+                break
+        }
+    }
+    
+    @MainActor private func postDataAsync(postTitle: String, postBody: String) {
         if postTitle == "" || postBody == "" {
             self.viewState = .badInput
             return;
@@ -31,5 +58,32 @@ class PostViewModel: ObservableObject {
                 self.viewState = .error
             }
         }
+    }
+    
+    private func postDataFuture(postTitle: String, postBody: String) {
+     
+        if postTitle == "" || postBody == "" {
+            self.viewState = .badInput
+            return;
+        }
+        
+        self.viewState = .loading
+        self.networkServiceFuture.postUserPost(UserPostData(userID: 1,
+                                                            postID: 1,
+                                                            postTitle: postTitle,
+                                                            postBody: postBody))
+        .sink(receiveCompletion: {[weak self] completion in
+            switch completion {
+                case .finished:
+                    break
+                case .failure(_):
+                    self?.viewState = .error
+            }
+        }, receiveValue: {[weak self] value in
+            
+            self?.viewState = .loaded
+            self?.postData = value
+        })
+        .store(in: &self.cancellables)
     }
 }
